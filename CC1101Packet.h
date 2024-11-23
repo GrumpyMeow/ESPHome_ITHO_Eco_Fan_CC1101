e8fdf780368bd7a902513787e7805094b11965a3
Meow <schutten@hotmail.com>
2024-11-23 13:07:59 +0100

    Update CC1101Packet.h

--- CC1101Packet.h
+++ CC1101Packet.h
@@ -1,15 +1,18 @@
 /*
- * Author: Klusjesman, modified bij supersjimmie for Arduino/ESP8266
+ * Author: Klusjesman, modified bij supersjimmie for Arduino/ESP8266/ESP32
  */
 
 
-#include <stdint.h>
-#include <stdio.h>
+#include <stdint.h>  // Added by Sander
+#include <stdio.h>   // Added by Sander
 
 #ifndef CC1101PACKET_H_
 #define CC1101PACKET_H_
 
-
+// #include <stdio.h> // commented by Sander 
+// #ifdef ESP8266     // commented by Sander
+// #include <Arduino.h>    // commented by Sander
+// #endif    // commented by Sander
 
 #define CC1101_BUFFER_LEN        64
 #define CC1101_DATA_LEN          CC1101_BUFFER_LEN - 3
